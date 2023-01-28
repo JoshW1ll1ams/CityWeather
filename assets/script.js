71dbe304ee0578baf7bc48339403301a41f8dd81
JoshW1ll1ams <117525232+JoshW1ll1ams@users.noreply.github.com>
2023-01-28 23:45:59 +0000

    added basic api calls

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,42 @@
+var apiKey = "880c668a67c5fcb81fdb410e76fe8763";
 var forecast = $("#forecast");
+var input = $(".form-input");
+var today = moment().format("GGGG-MM-DD HH:00:00");
+console.log(today)
+
+var city;
+var lat;
+var lon;
+
+$("#search-button").on("click", function() {
+city = input.val();
+var queryUrlGeocode = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+apiKey;
+$.ajax({
+  url: queryUrlGeocode,
+  method: "GET"
+}).then(function(response) {
+    var lat = response[0].lat;
+    var lon = response[0].lon;
+    var queryUrlWeather = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apiKey;
+
+    $.ajax({
+        url: queryUrlWeather,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    });
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
