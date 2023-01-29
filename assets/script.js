e933390cc4e67261d7c5ef0fb6316840eb87410d
JoshW1ll1ams <117525232+JoshW1ll1ams@users.noreply.github.com>
2023-01-29 00:36:56 +0000

    added main functionality

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
 var apiKey = "880c668a67c5fcb81fdb410e76fe8763";
 var forecast = $("#forecast");
 var input = $(".form-input");
-var today = moment().format("GGGG-MM-DD HH:00:00");
-console.log(today)
+var currentcity = $("#currentcity");
+var currenttemp = $("#currenttemp");
+var currentwind = $("#currentwind");
+var currenthumid = $("#currenthumid");
+
+
+
+var today = moment().format("GGGG-MM-DD");
+var todayDay = parseInt(moment().format("DD"))+1;
+
 
 var city;
 var lat;
 var lon;
+var counter;
 
 $("#search-button").on("click", function() {
 city = input.val();
@@ -24,6 +33,50 @@ $.ajax({
         method: "GET"
     }).then(function(response) {
         console.log(response)
+        for(var i =0; i<response.list.length;i++)
+            {
+                var date = response.list[i].dt_txt
+                var dayarr = date.split("-")
+                var day = dayarr[2].charAt(0)+dayarr[2].charAt(1)
+                
+                if(day.includes(todayDay))
+                    {
+                        var counter = i;
+                        break;
+                    }
+            }
+            currentcity.text(response.city.name)
+            currenttemp.text("Temp: "+response.list[0].main.temp)
+            currentwind.text("Wind Speed: "+response.list[0].wind.speed)
+            currenthumid.text("Humidity: "+response.list[0].main.humidity)
+
+            var addition = 0;
+            for(var i =0; i<5;i++)
+            {
+                
+                var forecastdiv = $('<div>');
+                var header = $('<h3>');
+                var icon = $('<img>');
+                var temp = $('<p>');
+                var wind = $('<p>');
+                var humidity = $('<p>');
+                
+                header.text(response.list[counter+addition].dt_txt);
+                temp.text("Temp: "+response.list[counter+addition].main.temp);
+                wind.text("Wind Speed: "+response.list[counter+addition].wind.speed);
+                humidity.text("Humidity: "+response.list[counter+addition].main.humidity);
+            
+                forecastdiv.addClass("forecastdiv")
+            
+                forecastdiv.append(header);
+                forecastdiv.append(icon);
+                forecastdiv.append(temp);
+                forecastdiv.append(wind);
+                forecastdiv.append(humidity);
+            
+                forecast.append(forecastdiv);
+                addition+=8;
+            }
     });
     });
 });
@@ -34,34 +87,3 @@ $.ajax({
 
 
 
-
-
-
-
-
-
-
-for(var i =0; i<5;i++)
-{
-    var forecastdiv = $('<div>');
-    var header = $('<h3>');
-    var icon = $('<img>');
-    var temp = $('<p>');
-    var wind = $('<p>');
-    var humidity = $('<p>');
-
-    header.text("1/1/1");
-    temp.text("Temp: 16.90");
-    wind.text("Wind: 16.90");
-    humidity.text("Humidity: 16.90");
-
-    forecastdiv.addClass("forecastdiv")
-
-    forecastdiv.append(header);
-    forecastdiv.append(icon);
-    forecastdiv.append(temp);
-    forecastdiv.append(wind);
-    forecastdiv.append(humidity);
-
-    forecast.append(forecastdiv);
-}
